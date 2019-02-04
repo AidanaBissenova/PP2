ed4555b57bd446cb8539e5e303ea4589822b0517
Aidana <Aidana-19.2000@mail.ru>
2019-02-04 13:43:44 +0600

    first commit

---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+            string[] tokens = Console.ReadLine().Split();
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = int.Parse(tokens[i]);
+            for(int i=0; i< arr.Length; i++)
+            {
+                Console.Write(arr[i] + " "   + arr[i] + " ");
+            }
+        Console.ReadKey();
+        }
+    }
+}
