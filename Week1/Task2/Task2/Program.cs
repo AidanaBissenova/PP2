f8888374cadf2a295af644fbf4ecc84eac3d31b4
Aidana <Aidana-19.2000@mail.ru>
2019-02-14 01:00:30 +0600

    Task2

---
+++ Week1/Task2/Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year;
+
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public Student()
+        {
+            name = Console.ReadLine();
+            id = Console.ReadLine();
+            year = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(id);
+            Console.WriteLine(year);
+        }
+        public void IncrementOfYear()
+        {
+            year++;
+            PrintInfo();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.IncrementOfYear();
+        }
+    }
+}
