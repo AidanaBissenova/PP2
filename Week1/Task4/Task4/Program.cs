c7ecfe54ae1a5aa2aaf6f616a6d63314708f44ee
Aidana <Aidana-19.2000@mail.ru>
2019-02-14 01:02:48 +0600

    Task4

---
+++ Week1/Task4/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); //ввод числа и конвертирование ее в тип integer
+            string[,] arr = new string[n, n]; //создаем двумерный массив
+            for(int i=0; i<n; i++) //создание цикла
+            {
+                for (int j = 0; j <= i; j++)//создание цикла
+                    arr[i, j] = "[*]"; //массив выводит [*] 
+            }
+            for (int i = 0; i < n; i++) //создание цикла
+            {
+                for (int j = 0; j <= i; j++) //создание цикла
+                    Console.Write(arr[i, j]); //выводим на консоль массив
+                Console.WriteLine(); //Выводим для того чтобы пройти на новую строку
+            }
+        }
+    }
+}
