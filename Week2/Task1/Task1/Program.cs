a1994d11d41650a12ff3b7451589652e219c639a
Aidana <Aidana-19.2000@mail.ru>
2019-02-16 03:20:00 +0600

    Task1

--- Week2/Task1/Task1/Program.cs
+++ Week2/Task1/Task1/Program.cs
@@ -11,16 +11,16 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            string text = File.ReadAllText(@"C:\Users\Acer\source\repos\Week2\Task1\Task1\input.txt");
-            for (int i = 0, j = text.Length - 1; i < j; i++, j--)
+            string text = File.ReadAllText(@"C:\Users\Acer\source\repos\Week2\Task1\Task1\input.txt");//считываем string через инпут файл
+            for (int i = 0, j = text.Length - 1; i < j; i++, j--) // цикл создан чтобы узнать является ли string палиндромом
             {
-                if (text[i] != text[j])
+                if (text[i] != text[j])//если эти буквы будут не одинаковыми (то есть допустим первая и последняя буква и тд) 
                 {
-                    Console.WriteLine("No");
-                    return;
+                    Console.WriteLine("No");//выводим нет
+                    return;//завершаем выполнение метода
                 }
             }
-            Console.WriteLine("Yes");
+            Console.WriteLine("Yes");// если же те буквы равны тогда выводим yes
         }
     }
 }
